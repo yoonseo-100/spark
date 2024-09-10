c6d99381aa080b9a7180f6c99704555745e5251a
cia999g <cia999g@gmail.com>
2024-09-10 14:26:06 +0900

    Delete 이슬기_spark directory

--- 이슬기_spark/code/js/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["배경화면1.jpeg","배경화면2.jpeg","배경화면3.jpeg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src=`img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
