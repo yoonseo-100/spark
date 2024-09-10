fd96a100c680d6919918206bc1c1d40603909f06
cia999g <cia999g@gmail.com>
2024-09-10 23:53:14 +0900

    2024-09-10 여섯번째 업데이트

--- 슬기/코코아클론_todolist_연구중/js/quiz.js
+++ 슬기/코코아클론_todolist_연구중/js/quiz.js
@@ -1,50 +1,49 @@
-const quotes = [
-    {
-        quote: "The way to get started is to quit talking and begin doing",
-        author: "Walt Disney",
-    },
-    {
-        quote: "Life is what happens when you're busy making other plans",
-        author: "John Lennon",
-    },
-    {
-        quote: "The world is a book and those who do not travel read only one page.",
-        author: "Saint Augustine",
-    },
-    {
-        quote: "The way to get started is to quit talking and begin doing",
-        author: "Walt Disney",
-    },
-    {
-        quote: "Life is what happens when you're busy making other plans",
-        author: "John Lennon",
-    },
-    {
-        quote: "The world is a book and those who do not travel read only one page.",
-        author: "Saint Augustine",
-    },
-    {
-        quote: "The way to get started is to quit talking and begin doing",
-        author: "Walt Disney",
-    },
-    {
-        quote: "Life is what happens when you're busy making other plans",
-        author: "John Lennon",
-    },
-    {
-        quote: "The world is a book and those who do not travel read only one page.",
-        author: "Saint Augustine",
-    },
-    {
-        quote: "The way to get started is to quit talking and begin doing",
-        author: "Walt Disney",
-    }
+const questionElement = document.querySelector("#quote span:first-child");
+const answerElement = document.querySelector("#quote span:last-child");
+const oButton = document.querySelector("#o-button");
+const xButton = document.querySelector("#x-button");
+const character = document.querySelector("#character");
+
+const questions = [
+    { question: "Is the Earth round?", answer: "O" },
+    { question: "Is 2 + 2 = 5?", answer: "X" },
+    { question: "Is the sky blue?", answer: "O" },
+    { question: "Is fire cold?", answer: "X" },
 ];
 
-const quote = document.querySelector("#quote span:first-child");
-const author = document.querySelector("#quote span:last-child");
+// 기본 문구 설정
+document.querySelector("#quiz-title").innerText = "[[O.X 퀴즈]]";
 
-const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
+// 랜덤한 질문 선택
+const randomQuestion = questions[Math.floor(Math.random() * questions.length)];
+questionElement.innerText = randomQuestion.question;
 
-quote.innerText = todayQuote.quote;
-author.innerText = todayQuote.author;
+// 정답을 체크하고 결과를 출력하는 함수
+function checkAnswer(userAnswer) {
+    if (userAnswer === randomQuestion.answer) {
+        answerElement.innerText = "Correct!";
+        showCharacter();  // 정답일 경우 캐릭터가 등장
+    } else {
+        answerElement.innerText = "Wrong!";
+    }
+}
+
+// 캐릭터가 나타나는 애니메이션 효과
+function showCharacter() {
+    character.classList.add("show");
+
+    // 5초 뒤 캐릭터 사라짐
+    setTimeout(() => {
+        character.classList.remove("show");
+    }, 5000);  // 5초 동안 캐릭터 표시 후 제거
+}
+
+// O 버튼 클릭 시 정답 체크
+oButton.addEventListener("click", () => {
+    checkAnswer("O");
+});
+
+// X 버튼 클릭 시 정답 체크
+xButton.addEventListener("click", () => {
+    checkAnswer("X");
+});
--- 슬기/코코아클론_todolist_연구중/js/weather.js
+++ 슬기/코코아클론_todolist_연구중/js/weather.js
@@ -1,19 +1,18 @@
-
-const API_KEY ="69aade5bc2ef530d39e773666f331598"
+const API_KEY = "69aade5bc2ef530d39e773666f331598";
 
 function onGeoOK(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("You live in",lat,lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch (url)
-        .then((Response)=>Response.json())
-        .then((data)=>{
+    
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
             const weather = document.querySelector("#weather span:first-child");
             const city = document.querySelector("#weather span:last-child");
             city.innerText = data.name;
-            weather.innerText = `${data.weather[0].main}/${data.main.temp}`;
-    });
+            weather.innerText = `${data.weather[0].main}`;
+        });
 }
 
 function onGeoError() {
