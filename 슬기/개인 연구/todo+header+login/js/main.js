c1a74889a52eb0df0a1a16938e0729e11f2c935c
cia999g <cia999g@gmail.com>
2024-09-13 16:07:04 +0900

    Add untracked files

---
+++ 슬기/개인 연구/todo+header+login/js/main.js
@@ -0,0 +1,8 @@
+const toogleBtn = document.querySelector('.navbar__toogleBtn');
+const menu = document.querySelector('.navbar__menu');
+const icons = document.querySelector('.navbar__icons');
+
+toogleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+});
