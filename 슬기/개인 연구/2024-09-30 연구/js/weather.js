5df2555d460a3aa8bcda5c9e27f75eee6466f15b
cia999g <cia999g@gmail.com>
2024-09-30 20:43:05 +0900

    2024-09-30 세번째 업데이트

--- 슬기/개인 연구/2024-09-30 연구/js/weather.js
+++ 슬기/개인 연구/2024-09-30 연구/js/weather.js
@@ -3,7 +3,7 @@ const API_KEY = "69aade5bc2ef530d39e773666f331598";
 function onGeoOK(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
     
     fetch(url)
         .then((response) => response.json())
@@ -11,7 +11,7 @@ function onGeoOK(position) {
             const weather = document.querySelector("#weather span:first-child");
             const city = document.querySelector("#weather span:last-child");
             city.innerText = data.name;
-            weather.innerText = `${data.weather[0].main}`;
+            weather.innerText = `${data.weather[0].description}`; // weather[0].main을 description으로 변경하여 더 자세한 날씨 설명 표시
         });
 }
 
@@ -20,3 +20,4 @@ function onGeoError() {
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
+
