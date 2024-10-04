8a4d117199ffc3c5491c54fe860dc9b7b1d7e23c
sel098 <limse615@gmail.com>
2024-10-04 15:45:42 +0900

    reactJS #6

--- test-react-app/src/App.js
+++ test-react-app/src/App.js
@@ -1,12 +1,29 @@
-import { useState } from "react";
-import Button from "./Button";
-import styles from "./app.module.css";
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("hi :>");
+    return () => console.log("bye :<");
+  }, []);
+  /* 위와 같은 코드임. 단지 위가 더 짧으니 위를 사용한다.
+  useEffect(function() {
+    console.log("hi :>");
+    return function() {
+      console.log("bye :<");
+    }
+  }, []);
+  */
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
