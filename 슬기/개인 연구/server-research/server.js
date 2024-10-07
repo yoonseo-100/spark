b25e1bfc362513f22944b7193ad428dd55338795
cia999g <cia999g@gmail.com>
2024-10-07 17:55:42 +0900

    2024-10-07 두번째 업데이트

--- 슬기/개인 연구/server-research/server.js
+++ 슬기/개인 연구/server-research/server.js
@@ -1,4 +1,18 @@
 const express = require ('express');
 const app = express();
 
-app.listen();
+app.listen(8080,function(){
+    console.log('listening on 8080')
+});
+
+app.get('/pet',function(요청, 응답){
+    응답.send('펫 용품 쇼핑할 수 있는 페이지입니다.');
+});
+
+app.get('/beauty',function(요청, 응답){
+    응답.send('뷰티 용품 쇼핑 페이지입니다.');
+});
+
+app.get('/', function(요청, 응답) {
+    응답.sendFile(__dirname + '/index.html');
+});
