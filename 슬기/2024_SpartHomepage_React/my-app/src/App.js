3e9174a750ed9eb2ba0b6b0bffe43a9521ab6433
cia999g <cia999g@gmail.com>
2024-10-21 22:37:16 +0900

    2024-10-21 Spart Homepage 위한 세번째 업데이트

--- 슬기/2024_SpartHomepage_React/my-app/src/App.js
+++ 슬기/2024_SpartHomepage_React/my-app/src/App.js
@@ -1,24 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Main from './Main';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Study <code> For Your Dream </code> In Spark Academy
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go to Spark
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* 기존 로고와 텍스트 */}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Study <code> For Your Dream </code> In Spark Academy
+          </p>
+
+          {/* Go to Spark 버튼 - 크기를 3배로 키우기 위해 CSS 클래스 추가 */}
+          <Link to="/main">
+            <button className="big-button">Go to Spark</button>
+          </Link>
+
+          {/* 라우트 설정 */}
+          <Routes>
+            <Route path="/main" element={<Main />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
---
+++ 슬기/2024_SpartHomepage_React/my-app/src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div>
+      <h1>Welcome to Spark Academy Main Page!</h1>
+      <p>Here is the main content of Spark Academy.</p>
+    </div>
+  );
+}
+
+export default Main;
