56beec54007f4039be467278fddf3993d3e0b5df
cia999g <cia999g@gmail.com>
2024-11-05 13:33:10 +0900

    2024-11-05 spark 처음부터

--- 슬기/Spark_Homepage/spark/src/App.js
+++ 슬기/Spark_Homepage/spark/src/App.js
@@ -1,29 +1,25 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import CustomNavbar from './components/CustomNavbar';
-import Home from './components/Home';
-import About from './components/About';
-import MainTeacher from './components/MainTeacher';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <CustomNavbar />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <main>
-        <h1>Welcome to Spark Edu</h1>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/main-teacher" element={<MainTeacher />} />
-        </Routes>
-      </main>
     </div>
   );
 }
 
 export default App;
-
-
