f291d71a2724c657a8dd2f95507b4bab269d38c1
cia999g <cia999g@gmail.com>
2024-11-11 21:06:09 +0900

    2024-11-11 첫번째 업데이트

---
+++ 슬기/Spark_변경/README.md
@@ -0,0 +1,3 @@
+## Project Demo : https://spark_edu.com
+
+### Project Video : https://www.youtube.com/spark_edu
---
+++ 슬기/Spark_변경/src/components/MainTeacher/MainTeacherHome.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Back from "../common/back/Back"
+import TeachersCard from "./MainTeacherCard"
+
+const CourseHome = () => {
+  return (
+    <>
+      <Back title='Explore Courses' />
+      <MainTeachercard/>
+    </>
+  )
+}
+
+export default CourseHome
