0fec0ec1fc2fa1ce1f6462b09a49ab1f91d9e44c
cia999g <cia999g@gmail.com>
2024-11-20 23:31:27 +0900

    2024-11-20 두번째 업데이트

--- 슬기/Spark/spark/src/Component/Common/Header/Header.jsx
+++ 슬기/Spark/spark/src/Component/Common/Header/Header.jsx
@@ -2,27 +2,41 @@ import React, { Component } from 'react';
 import './Header.css';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeMenuIndex: null, // 현재 활성화된 메뉴 인덱스
+    };
+  }
+
+  toggleMenu = (index) => {
+    this.setState((prevState) => ({
+      activeMenuIndex: prevState.activeMenuIndex === index ? null : index, // 동일한 메뉴를 클릭하면 닫힘
+    }));
+  };
+
   render() {
     const { language } = this.props;
+    const { activeMenuIndex } = this.state;
 
     const menuItems = language === 'KR'
       ? [
-          { href: '#home', text: '홈' },
-          { href: '#about', text: '회사소개' },
-          { href: '#services', text: '대표강사' },
-          { href: '#services', text: '강의 수강' },
-          { href: '#services', text: '구매' },
-          { href: '#services', text: '강의실' },
-          { href: '#contact', text: '문의' },
+          { href: '#home', text: '홈', details: ['세부사항1', '세부사항2', '세부사항3'] },
+          { href: '#about', text: '회사소개', details: ['세부사항1', '세부사항2', '세부사항3'] },
+          { href: '#services', text: '대표강사', details: ['세부사항1', '세부사항2', '세부사항3'] },
+          { href: '#services', text: '강의 수강', details: ['세부사항1', '세부사항2', '세부사항3'] },
+          { href: '#services', text: '구매', details: ['세부사항1', '세부사항2', '세부사항3'] },
+          { href: '#services', text: '강의실', details: ['세부사항1', '세부사항2', '세부사항3'] },
+          { href: '#contact', text: '문의', details: ['세부사항1', '세부사항2', '세부사항3'] },
         ]
       : [
-          { href: '#home', text: 'Home' },
-          { href: '#about', text: 'About' },
-          { href: '#services', text: 'Main Teacher' },
-          { href: '#services', text: 'Lecture' },
-          { href: '#services', text: 'Purchase' },
-          { href: '#services', text: 'Classroom' },
-          { href: '#contact', text: 'Contact' },
+          { href: '#home', text: 'Home', details: ['Detail 1', 'Detail 2', 'Detail 3'] },
+          { href: '#about', text: 'About', details: ['Detail 1', 'Detail 2', 'Detail 3'] },
+          { href: '#services', text: 'Main Teacher', details: ['Detail 1', 'Detail 2', 'Detail 3'] },
+          { href: '#services', text: 'Lecture', details: ['Detail 1', 'Detail 2', 'Detail 3'] },
+          { href: '#services', text: 'Purchase', details: ['Detail 1', 'Detail 2', 'Detail 3'] },
+          { href: '#services', text: 'Classroom', details: ['Detail 1', 'Detail 2', 'Detail 3'] },
+          { href: '#contact', text: 'Contact', details: ['Detail 1', 'Detail 2', 'Detail 3'] },
         ];
 
     return (
@@ -30,11 +44,23 @@ class Header extends Component {
         <a href="/" className="navbar-logo">SPARK_EDU</a>
         <ul className="navbar-links">
           {menuItems.map((item, index) => (
-            <li key={index}>
-              <a href={item.href}>{item.text}</a>
+            <li key={index} className="navbar-item">
+              <a href={item.href} onClick={() => this.toggleMenu(index)}>{item.text}</a>
+              {/* 세부 사항 표시 */}
+              {activeMenuIndex === index && (
+                <ul className="submenu">
+                  {item.details.map((detail, detailIndex) => (
+                    <li key={detailIndex}>{detail}</li>
+                  ))}
+                </ul>
+              )}
             </li>
           ))}
         </ul>
+        <div className="menu-icon">
+          {/* 메뉴 아이콘 */}
+          <span>☰</span>
+        </div>
       </nav>
     );
   }
